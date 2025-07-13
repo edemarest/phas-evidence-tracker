3e3513b801ce947b4e4fc30729612ccea7fd4de6
edemarest <demarest.e@northeastern.edu>
2025-07-13 12:02:49 -0400

    Initial commit for Phas Evidence Tracker (from Super4 commit 3335e6c3...)

---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  envDir: '../',
+  server: {
+    allowedHosts: [
+      'localhost',
+      'remarks-bread-breaking-anger.trycloudflare.com'
+    ],
+
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+    hmr: {
+      clientPort: 443,
+    },
+  },
+});
